5e1b26626cc3f438a7c710a016092681c345e5e1
Ivaylo Lafchiev <ivailolafchiev@yahoo.com>
2016-11-26 19:42:36 +0000

    working balance

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-"use strict";
 const knockKnockJokes = require('knock-knock-jokes');
 const cnApi = require('chuck-norris-api');
 const nessieSdk = require('nessie-nodejs-sdk')
 const talkify = require('talkify');
+const request = require('request');
+
 
 const Bot = talkify.Bot;
 
@@ -16,6 +17,8 @@ const Skill = BotTypes.Skill;
 const bot = new Bot();
 
 bot.trainAll([
+    new TrainingDocument('balance', 'balance'),
+
     new TrainingDocument('knock_joke', 'knock'),
     new TrainingDocument('knock_joke', 'knock knock'),
 
@@ -27,7 +30,7 @@ bot.trainAll([
     console.log(' BOT> Ready.');
 });
 
-const kJokeSkill = new Skill('my_knock_knock_joke_skill', 'knock_joke', function (context, request, response) {
+const kJokeSkill = new Skill('my_knock_knock_joke_skill', 'knock_joke', function (context, req, response) {
     if (!context.kJokes) {
         context.kJokes = [];
     }
@@ -47,13 +50,25 @@ const kJokeSkill = new Skill('my_knock_knock_joke_skill', 'knock_joke', function
     return response.send(new SingleLineMessage(newJoke));
 });
 
-const cJokeSkill = new Skill('my_chuck_norris_joke_skill', 'chuck_norris_joke', function(context, request, response) {
+const cJokeSkill = new Skill('my_chuck_norris_joke_skill', 'chuck_norris_joke', function(context, req, response) {
     return cnApi.getRandom().then(function(data) {
         return response.send(new SingleLineMessage(data.value.joke));
     });
 });
 
+const balanceSkill = new Skill('my_balance_skill', 'balance', function(context, req, response) {
+    var url = "http://api.reimaginebanking.com/customers/5839c5aa0fa692b34a9b8778/accounts?key=5e9a7df9497ab60eee4db8db8d16742d";
+    request.get(url,function(error,response,body){
+            if(error){
+                    console.log(error);
+            }else{
+                    console.log(JSON.parse(response.body)[0].balance);
+            }
+    });
+})
+
 bot.addSkill(kJokeSkill);
 bot.addSkill(cJokeSkill);
+bot.addSkill(balanceSkill);
 
 module.exports = bot;
